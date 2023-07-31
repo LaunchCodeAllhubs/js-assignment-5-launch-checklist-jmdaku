4b1b2c40f7650995453fb6cd6babb557c9316c7f
jmdaku <jmdaku@gmail.com>
2023-07-31 15:15:32 -0400

    edited syntax as attempted fix

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ const { myFetch , addDestinationInfo } = require("./scriptHelper");
 
 
 
-
-
 window.addEventListener("load", function() {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse = myFetch()
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
@@ -18,26 +16,20 @@ window.addEventListener("load", function() {
         console.log(listedPlanets);
             // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
         let planet = pickPlanet(listedPlanets);
-        return planet;
-
-        let name = "planet.name";
-        let diameter ="planet.diameter";
-        let star ="planet.star";
-        let distance = "planet.distance";
-        let moons = "planet.moons";
-        let imageURL = "planet.imageURL";     
         
-        addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl){
-
-        };
-
-          
-
+        let name = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let moons = planet.moons;
+        let image = planet.image;     
+        
+        addDestinationInfo(document, name, diameter, star, distance, moons, image);
+       
         
     });
 })
-   
-
+ 
 
 
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,21 +1,20 @@
-// Write your helper functions here!
+
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   /*
+      document.missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+                 `
 }
-//missionTarget
 
 function validateInput(testInput) {
     if(testInput === ""){
